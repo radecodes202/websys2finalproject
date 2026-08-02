36094528501443e3d87c134ce805e2f9dbc5a724
radecodes202 <radecodes202@gmail.com>
2026-08-02 22:26:28 +0800

    debug: print DJANGO_ENV and ALLOWED_HOSTS to stderr for debugging

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,7 +1,14 @@
 import os
 from .base import *
 
+import os
+import sys
+
 DJANGO_ENV = os.environ.get("DJANGO_ENV", "dev")
+
+# Print for debugging
+print(f"DJANGO_ENV: {DJANGO_ENV}", file=sys.stderr)
+print(f"ALLOWED_HOSTS from env: {os.environ.get('ALLOWED_HOSTS', 'NOT SET')}", file=sys.stderr)
 
 if DJANGO_ENV == "prod":
     from .prod import *
